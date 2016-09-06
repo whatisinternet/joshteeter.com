0d7c8884ac16f0551ac944166f34309c7b8406c2
Whatisinternet <joshteeter@gmail.com>
2016-09-06 00:10:51 -0400

    Added gzip

--- config/webpack.production.config.js
+++ config/webpack.production.config.js
@@ -1,5 +1,6 @@
 var base = require('./base.config.js');
 var webpack = require('webpack');
+var CompressionPlugin = require("compression-webpack-plugin");
 base.plugins.push(
   new webpack.optimize.DedupePlugin(),
   new webpack.DefinePlugin({
@@ -7,6 +8,13 @@ base.plugins.push(
       'NODE_ENV': JSON.stringify('production')
     }
   }),
-  new webpack.optimize.UglifyJsPlugin()
+  new webpack.optimize.UglifyJsPlugin(),
+  new CompressionPlugin({
+    asset: "[path].gz[query]",
+    algorithm: "gzip",
+    test: /\.js$|\.woff[2?]$|eot$\.ttf$|\.css$|\.html$/,
+    threshold: 10240,
+    minRatio: 0.8
+  })
 );
 module.exports = base;
